d9d66e3f2c353f52d4861a7e1f49cd5099f8c500
Levi Stovall <levi_stovall@yahoo.com>
2018-03-11 23:18:10 -0500

    set up to start adding testing for PrimeAssistant

---
+++ test/testPrimeAssistant/testPrimeAssistant.cpp
@@ -0,0 +1,18 @@
+#include "testPrimeAssistant.h"
+
+PrimeAssistantTest::PrimeAssistantTest()
+{}
+
+PrimeAssistantTest::~PrimeAssistantTest()
+{}
+
+void PrimeAssistantTest::SetUp()
+{}
+
+void PrimeAssistantTest::TearDown()
+{}
+
+
+
+
+
---
+++ test/testPrimeAssistant/testPrimeAssistant.h
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+#include "PrimeAssistant.h"
+
+// The fixture for testing class Rational.
+class PrimeAssistantTest : public ::testing::Test {
+
+protected:
+
+    // You can do set-up work for each test here.
+    PrimeAssistantTest();
+
+    // You can do clean-up work that doesn't throw exceptions here.
+    virtual ~PrimeAssistantTest();
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+    virtual void SetUp();
+
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+    virtual void TearDown();
+
+};
